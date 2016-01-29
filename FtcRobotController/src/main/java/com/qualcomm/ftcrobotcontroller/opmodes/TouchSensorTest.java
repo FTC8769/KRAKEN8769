e179dc93a8da6431aa64908a543503d9cb2b0e5c
David S <dmsvoboda@gmail.com>
2016-01-29 18:37:32 -0500

    Starting the Touch Sensor Superclass

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TouchSensorTest.java
@@ -0,0 +1,8 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+/**
+ * Created by svoboman on 1/29/16.
+ * Creating a SUPERCLASS for all of the Blue and Red auto-code
+ */
+public class TouchSensorTest {
+}
